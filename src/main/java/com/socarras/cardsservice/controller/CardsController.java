5e876551c1803422c442fcce22bac3f606bd942f
Alfonso <alfonso@mbp.local>
2024-02-06 10:11:04 -0500

    add apis to read config values

--- src/main/java/com/socarras/cardsservice/controller/CardsController.java
+++ src/main/java/com/socarras/cardsservice/controller/CardsController.java
@@ -1,6 +1,7 @@
 package com.socarras.cardsservice.controller;
 
 import com.socarras.cardsservice.constants.CardsConstants;
+import com.socarras.cardsservice.dto.CardsContactInfoDto;
 import com.socarras.cardsservice.dto.CardsDto;
 import com.socarras.cardsservice.dto.ErrorResponseDto;
 import com.socarras.cardsservice.dto.ResponseDto;
@@ -13,6 +14,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +28,21 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Cards API for EazyBank", description = "CRUD REST APIs for card details")
 public class CardsController {
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+    private final Environment environment;
+
     private final ICardsService cardsServiceImpl;
 
-    public CardsController(ICardsService cardsServiceImpl) {
+    private final CardsContactInfoDto cardsContactInfoDto;
+
+    public CardsController(Environment environment,
+                           ICardsService cardsServiceImpl,
+                           CardsContactInfoDto cardsContactInfoDto) {
+        this.environment = environment;
         this.cardsServiceImpl = cardsServiceImpl;
+        this.cardsContactInfoDto = cardsContactInfoDto;
     }
 
     @Operation(summary = "Create Card",
@@ -111,6 +125,40 @@ public class CardsController {
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @Operation(summary = "Get Build information", description = "Get Build information that is deployed into cards microservice")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @Operation(summary = "Get Java version", description = "Get Java versions details that is installed into cards microservice")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(summary = "Get Contact Info", description = "Contact Info details that can be reached out in case of any issues")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @GetMapping("/contact-info")
+    public ResponseEntity<CardsContactInfoDto> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(cardsContactInfoDto);
     }
 
 }
